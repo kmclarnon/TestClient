abdc531a6810e4bcf09a18d9a42baa65b3c60cc2
Kevin McLarnon <mclarnon.k@gmail.com>
2013-04-20 11:23:04 -0400

    Minor bugfix

--- src/TestClient.java
+++ src/TestClient.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -60,7 +58,7 @@ public class TestClient
     private static class Listener implements Runnable
     {
 
-        private static BufferedReader in;
+        private BufferedReader in;
         
         public Listener(BufferedReader in)
         {
